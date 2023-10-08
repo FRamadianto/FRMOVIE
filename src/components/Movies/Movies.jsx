a88d76949087b8a82dfdfb6bfa08f01943710f8f
Fikri Ramadianto <ramadiantofikri@gmail.com>
2023-10-09 00:53:33 +0700

    fix fetching data

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,16 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
-import { useSelector } from 'react-redux';
-
-import { useGetMoviesQuery } from '../../services/TMDB';
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+import { useGetMoviesQuery } from "../../services/TMDB";
+import { MovieList } from "..";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  console.log(data);
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for domething else.
+        </Typography>
+      </Box>
+    );
+  }
+  if (error) return "An error has occured";
   return (
     <div>
-      <h1>Movies</h1>
+      <MovieList movies={data} />
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
-export { default as Actors } from './Actors/Actors';
-export { default as Profile } from './Profile/Profile';
-export { default as Movies } from './Movies/Movies';
-export { default as NavBar } from './NavBar/NavBar';
-export { default as MovieInformation } from './MovieInformation/MovieInformation';
-export { default as SideBar } from './Sidebar/Sidebar';
+export { default as Actors } from "./Actors/Actors";
+export { default as Profile } from "./Profile/Profile";
+export { default as Movies } from "./Movies/Movies";
+export { default as MovieInformation } from "./MovieInformation/MovieInformation";
+export { default as SideBar } from "./Sidebar/Sidebar";
+export { default as MovieList } from "./MovieList/MovieList";
+export { default as NavBar } from "./NavBar/NavBar";
+export { default as Movie } from "./Movie/Movie";
